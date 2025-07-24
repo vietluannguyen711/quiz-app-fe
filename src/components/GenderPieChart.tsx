2208779e0c132f95decb37f94c95e9d23f9ec90c
vietluannguyen711 <vietluannguyen711@gmail.com>
2025-07-24 11:07:42 +0700

    Fix value in used

--- src/components/GenderPieChart.tsx
+++ src/components/GenderPieChart.tsx
@@ -24,7 +24,7 @@ export default function GenderPieChart() {
             outerRadius={80}
             dataKey="value"
           >
-            {data.map((entry, index) => (
+            {data.map((_, index) => (
               <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
             ))}
           </Pie>
